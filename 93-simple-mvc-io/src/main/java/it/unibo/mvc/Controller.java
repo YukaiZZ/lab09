a767a189b4b4589e1fb9a5bed942b08e50af9699
Yukai Zhou <yuaki.zhou@studio.unibo.it>
2023-12-04 19:34:13 +0100

    .gitattributes changed

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    final private File defaultFile = new File("user,home", "output.txt");
+    private File currentFile = defaultFile;
+
+    public void setCurrentFile(final File currentFile) {
+        this.currentFile = currentFile;
+    }
+
+    public File getCurrentFile(){
+        return currentFile;
+    }
+
+    public String getCurrentFilePath(){
+        return currentFile.getPath();
+    }
+
+    public void save(final String content) throws IOException{
+
+        try(FileWriter w = new FileWriter(this.currentFile))
+        {
+            w.write(content);
+        }
+    }
 }
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,19 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +21,45 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java graphical interface");
 
+
+    private SimpleGUI(Controller cl){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel p1 = new JPanel();
+        p1.setLayout(new BorderLayout());
+        final JTextArea textA = new JTextArea();
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(final ActionEvent event){
+                try {
+                    cl.save(textA.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "An error occurred", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        p1.add(textA, BorderLayout.CENTER);
+        p1.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(p1);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw/2 , sh/2);
+        frame.setLocationByPlatform(true);
+
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+
+    public static void main(final String... a) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 }
